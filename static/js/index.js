8823071331e9be8987429a3937b98dd9965c3a12
Eeman <eeman1adeniran@gmail.com>
2023-03-22 13:35:39 +0100

    Initial Commit

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+$(".tab").click(function () {
+  var tabs = $(this).parent().parent().children();
+  for (let i = 0; i < 3; i++) {
+    tab = $(tabs[i]).children();
+    if ($(tab).hasClass("active")){
+      $(tab).removeClass("active");
+    }
+    if ($(tab).attr("aria-current") === "page"){
+      $(tab).removeAttr("aria-current");
+    }
+  }
+  $(this).addClass("active");
+  $(this).attr("aria-current", "page")
+})
+
+var tabs = $('[data-tab-value]');
+var tabInfos = $('[data-tab-info]');
+
+for (let i = 0; i < 3; i++) {
+    $(tabs[i]).click(function () {
+        var target = $(tabs[i].dataset.tabValue);
+        for (let i = 0; i < 3; i++) {
+            $(tabInfos[i]).removeClass('active')
+        }
+        target.addClass('active');
+        return false;
+    })
+}
